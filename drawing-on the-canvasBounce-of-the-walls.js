a557967fb573f541accb583a399d2d1ba9603ae0
daves11 <35769301+daves11@users.noreply.github.com>
2018-03-06 15:37:05 +0000

    Update drawing-on the-canvasBounce-of-the-walls.js

--- drawing-on the-canvasBounce-of-the-walls.js
+++ drawing-on the-canvasBounce-of-the-walls.js
@@ -14,7 +14,7 @@ var dy = -2;
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, BallRadius, 0, Math.PI*2);
-    ctx.fillStyle = "OO95DD";
+    ctx.fillStyle = "#OO95DD";
     ctx.fill();
     ctx.closePath();
 }
